01cb2c7e6e346f5001c8c8677457e2b566a40e15
jamesrom <j.romeril@gmail.com>
2023-10-26 01:04:00 +1100

    Prepare v1.0.0

--- publish.ps1
+++ publish.ps1
@@ -2,5 +2,8 @@ cd .\internal
 . .\build.ps1
 cd ..
 
-docker run --rm -it -v ${PWD}:/workspace vsce package --pre-release
-docker run --rm -it -v ${PWD}:/workspace vsce publish --pre-release
+# docker run --rm -it -v ${PWD}:/workspace vsce package --pre-release
+# docker run --rm -it -v ${PWD}:/workspace vsce publish --pre-release
+
+docker run --rm -it -v ${PWD}:/workspace vsce package
+docker run --rm -it -v ${PWD}:/workspace vsce publish
